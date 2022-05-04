94a80b73825e31237ad5439accfb65b3ae0c1460
Nishant-Pall <nishant@loopinhq.com>
2022-05-04 14:39:10 +0530

    Match input with rng result

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+    println!("Input your guess");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please enter a valid number");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("To small"),
+        Ordering::Greater => println!("To Big"),
+        Ordering::Equal => println!("Yo win"),
+    }
 }
